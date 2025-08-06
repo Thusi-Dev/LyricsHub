baf9f3b2163830f9a1de6e6de1c82fe6a9c84059
Thusi Dev <6880576+Thusi-Dev@users.noreply.github.com>
2025-08-06 12:38:29 +0200

    Update settings.py

--- music_lyrics/settings.py
+++ music_lyrics/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'rest_framework',
-	'lyrics',
+'rest_framework',
+'lyrics',
 ]
 
 MIDDLEWARE = [
