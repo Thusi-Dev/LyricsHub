95f5b0e98f9e89dbe1d6bf1cfdaff6e878870621
Thusi-Dev <thusilob@gmail.com>
2025-08-06 23:47:01 +0200

    Added urls for SongView and LyricsView

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -1,11 +1,13 @@
 # lyrics/urls.py
 from django.urls import path
-from .views import SongList, LyricsList, MusicRecognitionView, index
+from .views import SongList, LyricsList, MusicRecognitionView, index, SongView, LyricsView
 
 
 urlpatterns = [
     path('songs/', SongList.as_view()),
     path('lyrics/', LyricsList.as_view()),
-	path('recognize-music/',MusicRecognitionView.as_view()),
-	path('', index, name='index'),
+    path('recognize-music/',MusicRecognitionView.as_view()),
+    path('', index, name='index'),
+    path('songs/<int:song_id>/', SongView.as_view()),
+    path('songs/<int:song_id>/lyrics/', LyricsView.as_view()),
 ]
